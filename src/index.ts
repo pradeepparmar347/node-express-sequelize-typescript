8357d3e5ff372aa888365c11c56d7cd24df77430
Pradeep Parmar <pradeepparmar347@gmail.com>
2022-09-29 17:31:48 +0530

    Added not found route

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import Logger from './lib/logger';
+import express, { Express, Request, Response } from 'express';
 import morganMiddleware from './config/morganMiddleware';
+import Logger from './lib/logger';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 app.use(morganMiddleware);
+const baseUrl = process.env.BASE_URL || '/';
+const apiBaseUrl = process.env.API_BASE_URL || '/api/';
 
-app.get('/', (req: Request, res: Response) => {
+app.get(baseUrl, (req: Request, res: Response) => {
+  res.send('⚡️Express + TypeScript Server');
+});
+
+app.get(apiBaseUrl, (req: Request, res: Response) => {
+  res.send('⚡️Express + TypeScript apis');
+});
+
+app.get(baseUrl + 'logging', (req: Request, res: Response) => {
   Logger.error('This is an error log');
   Logger.warn('This is a warn log');
   Logger.info('This is a info log');
   Logger.http('This is a http log');
   Logger.debug('This is a debug log');
-  res.send('Express + TypeScript Server');
+  res.send('⚡️Express + TypeScript Server');
+});
+
+app.get('*', function (req: Request, res: Response) {
+  res.send('The resource you are looking for is not found.');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
